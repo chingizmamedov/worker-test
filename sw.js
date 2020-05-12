41a6959043a8daeb6bb7bd668384ebb5855ea5fe
chuxani <chuxani@gmail.com>
2020-05-13 00:39:53 +0400

    add video

--- sw.js
+++ sw.js
@@ -1,9 +1,27 @@
+const cacheName = "v1"
+
+const assets = [
+  "/",
+  "index.html",
+  "/css/style.css",
+  "/js/script.js"
+]
+
+
 // install event
 self.addEventListener("install", (evt) => {
   console.log("service worker installed", evt);
+
+  evt.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      return cache.addAll(
+        assets
+      );
+    })
+  );
 });
 
-// activate event -- new
+// activate event
 self.addEventListener("activate", (evt) => {
   console.log("service worker activated", evt);
 });
@@ -11,4 +29,14 @@ self.addEventListener("activate", (evt) => {
 // fetch events
 self.addEventListener("fetch", (evt) => {
   console.log("evt", evt);
+  evt.respondWith(
+    caches.open(cacheName).then(function(cache) {
+      return cache.match(evt.request).then(function (response) {
+        return response || fetch(evt.request).then(function(response) {
+          cache.put(evt.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
 });

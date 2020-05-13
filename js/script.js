261e7ca1d413b6a3e4de39cff38390e2a8963325
Chingiz Mamedov <38461842+chingiz92mi@users.noreply.github.com>
2020-05-13 10:43:35 +0400

    set new url

--- js/script.js
+++ js/script.js
@@ -1,29 +1,26 @@
-let slides = []
+let slides = [];
 slidesUrls = [];
 
 fetch("http://dsapi.testqmeter.net/v1/display/samsam")
-    .then(reponse => reponse.json())
-    .then(reponse => {
-        console.log(reponse)
-        slides = reponse.playlist.slides;
-        slidesUrls = slides[0].items.map(item => item.src)
-        console.log("slidesUrls", slidesUrls)
-        console.log("slides", slides)
-    })
-    .then(() => {
-
-        slidesUrls.forEach(elementItem => {
-            console.log("element", elementItem)
-            let element = document.createElement("video")
-            element.id = "video"
-            element.width = "400"
-            element.height = "300"
-            element.setAttribute("autoplay", true)
-            element.setAttribute("controls", true)
-            let elementSource = document.createElement("source")
-            elementSource.src = elementItem;
-            element.append(elementSource);
-            console.log("element", element);
-            document.getElementById("video-wrap").append(element);
-        });
-    })
+	.then((reponse) => reponse.json())
+	.then((reponse) => {
+		console.log(reponse);
+		slides = reponse.playlist.slides;
+		slidesUrls = slides[1].items.map((item) => item.src);
+	})
+	.then(() => {
+		slidesUrls.forEach((elementItem) => {
+			console.log("element", elementItem);
+			let element = document.createElement("video");
+			element.id = "video";
+			element.width = "400";
+			element.height = "300";
+			element.setAttribute("autoplay", true);
+			element.setAttribute("controls", true);
+			let elementSource = document.createElement("source");
+			elementSource.src = elementItem;
+			element.append(elementSource);
+			console.log("element", element);
+			document.getElementById("video-wrap").append(element);
+		});
+	});
--- sw.js
+++ sw.js
@@ -1,42 +1,37 @@
-const cacheName = "v1"
+const cacheName = "v1";
 
-const assets = [
-  "/",
-  "index.html",
-  "/css/style.css",
-  "/js/script.js"
-]
-
+const assets = ["/", "index.html", "/css/style.css", "/js/script.js"];
 
 // install event
 self.addEventListener("install", (evt) => {
-  console.log("service worker installed", evt);
+	console.log("service worker installed", evt);
 
-  evt.waitUntil(
-    caches.open(cacheName).then(function(cache) {
-      return cache.addAll(
-        assets
-      );
-    })
-  );
+	evt.waitUntil(
+		caches.open(cacheName).then(function (cache) {
+			return cache.addAll(assets);
+		}),
+	);
 });
 
 // activate event
 self.addEventListener("activate", (evt) => {
-  console.log("service worker activated", evt);
+	console.log("service worker activated", evt);
 });
 
 // fetch events
 self.addEventListener("fetch", (evt) => {
-  console.log("evt", evt);
-  evt.respondWith(
-    caches.open(cacheName).then(function(cache) {
-      return cache.match(evt.request).then(function (response) {
-        return response || fetch(evt.request).then(function(response) {
-          cache.put(evt.request, response.clone());
-          return response;
-        });
-      });
-    })
-  );
+	// console.log("evt", evt);
+	evt.respondWith(
+		caches.open(cacheName).then(function (cache) {
+			return cache.match(evt.request).then(function (response) {
+				return (
+					response ||
+					fetch(evt.request).then(function (response) {
+						cache.put(evt.request, response.clone());
+						return response;
+					})
+				);
+			});
+		}),
+	);
 });
